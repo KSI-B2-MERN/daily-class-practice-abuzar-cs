28705d4036d077c3d4bcbfc9d143c7fc9a2d7e7d
abuzar-cs <abuzarcharsadda1@gmail.com>
2023-09-20 16:42:22 +0500

    component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import './App.css';
 
+const Job = (props) => {
+  return (
+     <div> 
+      <h1>{props.salary} </h1>
+      <h1>{props.position} </h1>
+      <h1>{props.company} </h1>
+      </div>
+    
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="">hello world</header>
+     <Job salary={2000}  position="ceo" company="netflix"  />
+     <Job salary={3000}  position="c-ceo" company="oneman"  />
+     <Job salary={4000}  position="ceo-d" company="absolutions"  />
     </div>
   );
+  
+
+
+ 
 }
 
 export default App;
