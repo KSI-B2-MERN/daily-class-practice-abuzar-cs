4b5eb29b4aafe87bd9a1ecccaa128189b17f3010
abuzar-cs <abuzarcharsadda1@gmail.com>
2023-10-09 15:47:03 +0500

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
 
-function App(){
-// let carColor= "black";
-const [carColour, setCarcolor] = useState("black")
-return (
-  <div>
-    <button onClick={()=>{
-      setCarcolor(carColour=== "black" ? "red" : "black")
-    }}>change color</button>
-    <h1 style={{color: carColour}}>the car color is {carColour}</h1>
-  </div>
-)
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+    setNewTask("");
+  }
+
+  return (
+    <div className="container mx-auto mt-10">
+      <h1 className="text-2xl font-semibold mb-4">Todo List</h1>
+      <div className="flex">
+        <input
+          className="border rounded-l py-2 px-4 w-4/5"
+          type="text"
+          value={newTask}
+          onChange={handleChange}
+        />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r"
+          onClick={addTask}
+        >
+          Add Task
+        </button>
+      </div>
+      <div className="mt-4">
+        {todoList.map((task, index) => (
+          <div
+            key={index}
+            className="bg-gray-200 rounded-md p-2 mb-2 flex justify-between items-center"
+          >
+            <span>{task}</span>
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+              onClick={() => {
+                const updatedList = [...todoList];
+                updatedList.splice(index, 1);
+                setTodoList(updatedList);
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default App;
 
@@ -24,8 +62,69 @@ export default App;
 
 
 
+// import React from 'react';
+// import { useState } from 'react';
+// import './App.css';
 
 
+// function App(){
+// const [todoList , setTodoList] = useState([])
+// const[newTask , setNewTask] = useState("")
+// const handleChange = (event) => {
+//   setNewTask(event.target.value);
+// }
+// const AddTask = () => {
+//   setTodoList([...todoList,newTask]);
+//   console.log(todoList);
+// }
+// return (
+//   <div>
+//     <input onChange={handleChange} />
+//     <button onClick={AddTask}>Add New Task</button>
+//     <div className='list'>
+//       {todoList.map((task,index)=>(
+//         <h1 key={index}>{task}</h1>
+//       ))}
+//      </div>
+//   </div>
+  
+// );
+// }
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//use state
+// let carColor= "black";
+// const [carColour, setCarcolor] = useState("black")
+// return (
+//   <div>
+//     <button onClick={()=>{
+//       setCarcolor(carColour=== "black" ? "red" : "black")
+//     }}>change color</button>
+//     <h1 style={{color: carColour}}>the car color is {carColour}</h1>
+//   </div>
+// )
+// }
 
 // mapping & ternary operator
 
